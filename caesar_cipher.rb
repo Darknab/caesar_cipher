4fefe7b83d76d4f961893d7b2e56771cbbf2397b
Darknab <darknab@live.fr>
2023-02-12 10:13:30 +0100

    Modified the code so the check_overflow work correctly

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,15 +2,19 @@ def cipher(text, shift)
     ascii_text = text.codepoints
     encrypted = []
     ascii_text.each_with_index do |element, index|
-        if (element >= 97 && element <= 122 ) 
-            encrypted[index] = element + shift
-            check_overflow(encrypted[index], 97, 122)
+        puts "I'm #{element.chr}, in index #{index}"
+        if (element >= 97 && element <= 122 )
+            puts "I'm #{element.chr} and i'm in the downcase if" 
+            encrypted[index] = check_overflow((element + shift), 97, 122)
+            puts "This is for downcase #{encrypted[index]}"
         elsif (element >= 65 && element <= 90)
-            encrypted[index] = element + shift
-            check_overflow(encrypted[index], 65, 90)
+            puts "I'm #{element.chr} and i'm in the upcase if" 
+            encrypted[index] = check_overflow((element + shift), 65, 90)
+            puts "This is for upcase #{encrypted[index]}"
         else encrypted[index] = element
         end 
     end
+    puts encrypted
     result = []
     encrypted.each_with_index { |element,index| result[index] = element.chr}
     puts result.join
@@ -21,6 +25,7 @@ def check_overflow(element,min, max)
         element = element + 26
     elsif element > max
         element = element -26
+    else element = element
     end
 end
 
