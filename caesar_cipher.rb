59eab532410c314ed5da8f1ffc93b0a2c5aa1b8b
Darknab <darknab@live.fr>
2023-02-12 10:34:40 +0100

    added a test that prevents the user from entering an incorrect shift, and removed all the puts used for debugging

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,19 +2,13 @@ def cipher(text, shift)
     ascii_text = text.codepoints
     encrypted = []
     ascii_text.each_with_index do |element, index|
-        puts "I'm #{element.chr}, in index #{index}"
         if (element >= 97 && element <= 122 )
-            puts "I'm #{element.chr} and i'm in the downcase if" 
             encrypted[index] = check_overflow((element + shift), 97, 122)
-            puts "This is for downcase #{encrypted[index]}"
         elsif (element >= 65 && element <= 90)
-            puts "I'm #{element.chr} and i'm in the upcase if" 
             encrypted[index] = check_overflow((element + shift), 65, 90)
-            puts "This is for upcase #{encrypted[index]}"
         else encrypted[index] = element
         end 
     end
-    puts encrypted
     result = []
     encrypted.each_with_index { |element,index| result[index] = element.chr}
     puts result.join
@@ -31,7 +25,10 @@ end
 
 puts "Please enter a text to encrypt"
 text = gets.chomp
-puts "Please choose a shift"
+puts "Please choose a shift (an integer between - 26 and 26)"
 shift = gets.chomp.to_i
-
-cipher(text, shift)
+while shift > 26 || shift < -26
+    puts "out of range, please inter a correct number for the shift"
+    shift = gets.chomp.to_i   
+end
+cipher(text, shift)   
