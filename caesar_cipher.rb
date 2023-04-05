80d1167857f052cf91690b72ee6951c87d885cb8
Darknab <darknab@live.fr>
2023-04-05 10:28:06 +0100

    Refoactor code using Rubocop

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,34 +1,36 @@
 def cipher(text, shift)
-    ascii_text = text.codepoints
-    encrypted = []
-    ascii_text.each_with_index do |element, index|
-        if (element >= 97 && element <= 122 )
-            encrypted[index] = check_overflow((element + shift), 97, 122)
-        elsif (element >= 65 && element <= 90)
-            encrypted[index] = check_overflow((element + shift), 65, 90)
-        else encrypted[index] = element
-        end 
-    end
-    result = []
-    encrypted.each_with_index { |element,index| result[index] = element.chr}
-    puts result.join
+  ascii_text = text.codepoints
+  encrypted = []
+  ascii_text.each_with_index do |element, index|
+    encrypted[index] = if element >= 97 && element <= 122
+                         check_overflow((element + shift), 97, 122)
+                       elsif element >= 65 && element <= 90
+                         check_overflow((element + shift), 65, 90)
+                       else
+                         element
+                       end
+  end
+  result = []
+  encrypted.each_with_index { |element, index| result[index] = element.chr }
+  puts result.join
 end
 
-def check_overflow(element,min, max)
-    if element < min
-        element = element + 26
-    elsif element > max
-        element = element -26
-    else element = element
-    end
+def check_overflow(element, min, max)
+  element = if element < min
+              element + 26
+            elsif element > max
+              element - 26
+            else
+              element
+            end
 end
 
-puts "Please enter a text to encrypt"
+puts 'Please enter a text to encrypt'
 text = gets.chomp
-puts "Please choose a shift (an integer between - 26 and 26)"
+puts 'Please choose a shift (an integer between - 26 and 26)'
 shift = gets.chomp.to_i
 while shift > 26 || shift < -26
-    puts "out of range, please inter a correct number for the shift"
-    shift = gets.chomp.to_i   
+  puts 'out of range, please inter a correct number for the shift'
+  shift = gets.chomp.to_i
 end
-cipher(text, shift)   
+cipher(text, shift)
